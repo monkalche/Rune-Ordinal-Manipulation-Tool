bd99a74cf833a77034c374fe28dc75ea23326e06
savvydev84 <raymodncasy@gmail.com>
2024-07-24 15:12:16 +0900

    Feat:pesudo-psbt-for-transactoin-fee

--- Rune_airdrop.ts
+++ Rune_airdrop.ts
@@ -1,51 +1,51 @@
-// import * as Bitcoin from "bitcoinjs-lib";
-// import ecc from "@bitcoinerlab/secp256k1";
-// import {
-//   SEED,
-//   STANDARD_RUNE_UTXO_VALUE,
-//   TESTNET,
-//   networkType,
-// } from "../../config/config";
-// import { IUtxo } from "../../utils/types";
-// import { RuneId, Runestone, none } from "runelib";
-// import initializeWallet from "../wallet/initializeWallet";
-// import { SeedWallet } from "../wallet/SeedWallet";
-// import app from "../..";
-// Bitcoin.initEccLib(ecc);
+import * as Bitcoin from "bitcoinjs-lib";
+import ecc from "@bitcoinerlab/secp256k1";
+import {
+  SEED,
+  STANDARD_RUNE_UTXO_VALUE,
+  TESTNET,
+  networkType,
+} from "../../config/config";
+import { IUtxo } from "../../utils/types";
+import { RuneId, Runestone, none } from "runelib";
+import initializeWallet from "../wallet/initializeWallet";
+import { SeedWallet } from "../wallet/SeedWallet";
+import app from "../..";
+Bitcoin.initEccLib(ecc);
 
-// // Create dummy psbt for buyer offer
-// export const SameRuneTransferTx = (
-//   addressList: Array<string>,
-//   amount: number,
-//   rune_id: string,
-//   networkType: string,
-//   runeUtxo: IUtxo
-// ): string => {
-//   // Initialize seed Wallet
-//   const wallet: SeedWallet = initializeWallet(
-//     networkType,
-//     SEED,
-//     app.locals.walletIndex
-//   );
+// Create dummy psbt for buyer offer
+export const SameRuneTransferTx = (
+  addressList: Array<string>,
+  amount: number,
+  rune_id: string,
+  networkType: string,
+  runeUtxo: IUtxo
+): string => {
+  // Initialize seed Wallet
+  const wallet: SeedWallet = initializeWallet(
+    networkType,
+    SEED,
+    app.locals.walletIndex
+  );
 
-//   // Create psbt instance
-//   const psbt = new Bitcoin.Psbt({
-//     network:
-//       networkType == TESTNET
-//         ? Bitcoin.networks.testnet
-//         : Bitcoin.networks.bitcoin,
-//   });
+  // Create psbt instance
+  const psbt = new Bitcoin.Psbt({
+    network:
+      networkType == TESTNET
+        ? Bitcoin.networks.testnet
+        : Bitcoin.networks.bitcoin,
+  });
 
-//   // Input all buyer Rune UTXOs for rune token
-//   psbt.addInput({
-//     hash: runeUtxo.txid,
-//     index: runeUtxo.vout,
-//     witnessUtxo: {
-//       value: runeUtxo.value,
-//       script: wallet.output,
-//     },
-//     tapInternalKey: Buffer.from(wallet.publicKey, "hex").subarray(1, 33),
-//   });
+  // Input all buyer Rune UTXOs for rune token
+  psbt.addInput({
+    hash: runeUtxo.txid,
+    index: runeUtxo.vout,
+    witnessUtxo: {
+      value: runeUtxo.value,
+      script: wallet.output,
+    },
+    tapInternalKey: Buffer.from(wallet.publicKey, "hex").subarray(1, 33),
+  });
 
 //   // Create Runestone
 //   const edicts: any = [];
